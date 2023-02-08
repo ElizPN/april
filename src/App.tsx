e58e770daf49519aed59fc95cb98e7c4c5ecd00e
ElizPN <pankevych.liza@gmail.com>
2023-02-08 14:49:36 +0100

    add About section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Clients } from "./components/Clients";
+import { About } from "./components/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <Clients />
+      <About />
     </div>
   );
 }
