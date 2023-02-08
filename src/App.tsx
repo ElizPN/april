9d584b110a86b9d4218ae6da8d249cdd15fa619d
ElizPN <pankevych.liza@gmail.com>
2023-02-08 14:20:26 +0100

    move components to separate folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Header } from "./Header";
-import { Hero } from "./Hero";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Clients } from "./components/Clients";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Clients />
     </div>
   );
 }
