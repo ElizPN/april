0c0f373da8c98403d9002109a4d6e1490014d4ea
ElizPN <pankevych.liza@gmail.com>
2023-02-12 13:29:12 +0100

    remove unused chapters

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ export function Header() {
             </li>
             <li>
               <a className='nav-link scrollto' href='#services'>
-                Services
+                Video
               </a>
             </li>
             <li>
@@ -38,53 +38,8 @@ export function Header() {
             </li>
             <li>
               <a className='nav-link scrollto' href='#team'>
-                Team
+                Our team
               </a>
-            </li>
-            <li>
-              <a href='blog.html'>Blog</a>
-            </li>
-            <li className='dropdown'>
-              <a href='#'>
-                <span>Drop Down</span> <i className='bi bi-chevron-down'></i>
-              </a>
-              <ul>
-                <li>
-                  <a href='#'>Drop Down 1</a>
-                </li>
-                <li className='dropdown'>
-                  <a href='#'>
-                    <span>Deep Drop Down</span>{" "}
-                    <i className='bi bi-chevron-right'></i>
-                  </a>
-                  <ul>
-                    <li>
-                      <a href='#'>Deep Drop Down 1</a>
-                    </li>
-                    <li>
-                      <a href='#'>Deep Drop Down 2</a>
-                    </li>
-                    <li>
-                      <a href='#'>Deep Drop Down 3</a>
-                    </li>
-                    <li>
-                      <a href='#'>Deep Drop Down 4</a>
-                    </li>
-                    <li>
-                      <a href='#'>Deep Drop Down 5</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href='#'>Drop Down 2</a>
-                </li>
-                <li>
-                  <a href='#'>Drop Down 3</a>
-                </li>
-                <li>
-                  <a href='#'>Drop Down 4</a>
-                </li>
-              </ul>
             </li>
             <li>
               <a className='nav-link scrollto' href='#contact'>
@@ -94,10 +49,6 @@ export function Header() {
           </ul>
           <i className='bi bi-list mobile-nav-toggle'></i>
         </nav>
-
-        <a href='#about' className='get-started-btn scrollto'>
-          Get Started
-        </a>
       </div>
     </header>
   );
