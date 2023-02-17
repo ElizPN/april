950e1fc645535ec220c55c4cabfe3b0fb6b7651b
ElizPN <pankevych.liza@gmail.com>
2023-02-17 13:30:35 +0100

    create data strctre for nav Items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,73 +1,15 @@
 import React, { useState } from "react";
 
-// export function Header() {
-//   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+//create array from objects
 
-//   const handleMobileNavToggle = () => {
-//     setIsMobileNavOpen(!isMobileNavOpen);
-//   };
-//   return (
-//     <header
-//       id='header'
-//       className={`fixed-top d-flex align-items-center ${
-//         isMobileNavOpen ? "navbar-mobile" : ""
-//       }`}
-//     >
-//       <div className='container d-flex align-items-center'>
-//         <h1 className='logo me-auto'>
-//           <a href='index.html'>
-//             April<span>.</span>
-//           </a>
-//         </h1>
-
-//         <a href='index.html' className='logo me-auto'>
-//           <img src='assets/img/logo.png' alt='' />
-//         </a>
-
-//         <nav id='navbar' className='navbar order-last order-lg-0'>
-//           <ul>
-//             <li>
-//               <a className='nav-link scrollto active' href='#hero'>
-//                 Home
-//               </a>
-//             </li>
-//             <li>
-//               <a className='nav-link scrollto' href='#program'>
-//                 Our program
-//               </a>
-//             </li>
-//             <li>
-//               <a className='nav-link scrollto' href='#services'>
-//                 Video
-//               </a>
-//             </li>
-//             <li>
-//               <a className='nav-link scrollto ' href='#portfolio'>
-//                 Portfolio
-//               </a>
-//             </li>
-//             <li>
-//               <a className='nav-link scrollto' href='#team'>
-//                 Our team
-//               </a>
-//             </li>
-//             <li>
-//               <a className='nav-link scrollto' href='#contact'>
-//                 Contact
-//               </a>
-//             </li>
-//           </ul>
-//           <i
-//             className={`bi bi-list mobile-nav-toggle ${
-//               isMobileNavOpen ? "bi-x" : "bi-list"
-//             }`}
-//             onClick={handleMobileNavToggle}
-//           ></i>
-//         </nav>
-//       </div>
-//     </header>
-//   );
-// }
+const navItems = [
+  { name: "Home", link: "#hero" },
+  { name: "Our program", link: "#program" },
+  { name: "Listen", link: "#services" },
+  { name: "Portfolio", link: "#portfolio" },
+  { name: "Or team", link: "#team" },
+  { name: "Contact", link: "#contact" },
+];
 
 export function Header() {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
@@ -84,11 +26,10 @@ export function Header() {
             April<span>.</span>
           </a>
         </h1>
-
         <a href='index.html' className='logo me-auto'>
           <img src='assets/img/logo.png' alt='' />
         </a>
-
+        {/* loop array and render jsx with all styles */}
         <nav
           id='navbar'
           className={`navbar order-last order-lg-0 ${
@@ -108,7 +49,7 @@ export function Header() {
             </li>
             <li>
               <a className='nav-link scrollto' href='#services'>
-                Video
+                Listen
               </a>
             </li>
             <li>
